5321940773a5d61d3bce9f5ebf2bfebd6f621df8
Abs-def <archisman64@gmail.com>
2023-02-16 03:09:18 +0530

    initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>
+                <h3>Food Rs10</h3>
+                <h3>Petrol Rs100</h3>
+                <h3>MOvie Rs200</h3>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
