23d41a2a428cdf04c2c141edcc2839797d292dc8
Abs-def <archisman64@gmail.com>
2023-02-24 00:20:36 +0530

    dom manipulation using states

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,14 +1,20 @@
 import "./ExpenseItem.css";
-
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
 export function ExpenseItem(props) {
+  const [amount, setAmount] = useState(props.amount);
+
+  const clickHandler = () => {
+    setAmount(100)
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails title={props.title} amount={props.amount} localtion={props.location}/>
+      <ExpenseDetails title={props.title} amount={amount} localtion={props.location}/>
+      <button onClick={clickHandler}>Update Expense</button>
     </Card>
   );
 }
